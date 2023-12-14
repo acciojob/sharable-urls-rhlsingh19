492b9f4038993ee1a48a402b1cc4348de2ea4bcd
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2023-12-14 20:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+document.getElementById('myForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const name = document.getElementById('name').value;
+      const year = document.getElementById('year').value;
+
+      const url = new URL(document.getElementById('url').textContent);
+      url.searchParams.set('name', name);
+      url.searchParams.set('year', year);
+
+      document.getElementById('url').textContent = url.toString();
+    });
